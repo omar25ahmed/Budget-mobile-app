f6b1814c24e1a5c27359ad110a774ec2279e4fb1
Omar Ragheb <ahmed.raph29@gmail.com>
2022-07-01 00:56:31 +0200

    Add heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,8 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  config.require_master_key = true
+  config.action_mailer.default_url_options = { host: 'https://budget-mobile-omar.herokuapp.com/' }
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
