eb01989251542ef4cb336f7efb1662e705f20ee9
Omar Ragheb <ahmed.raph29@gmail.com>
2022-06-30 23:48:03 +0200

    Add the integertion/unit tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :deals, foreign_key: :user_id, class_name: 'Deal', dependent: :destroy
   has_many :categories, foreign_key: :user_id, class_name: 'Category', dependent: :destroy
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    before(:each) do
+      @user = User.create(name: 'Jane Doe', email: 'a@a', password: '123456', confirmed_at: Time.now)
+      @category = Category.create(name: 'Sport', icon: 'icon', user_id: @user.id)
+
+    end
+    it 'should be valid' do
+      expect(@user).to be_valid
+    end
+    it 'should not be valid without name' do
+      @user.name = nil
+      expect(@user).to_not be_valid
+    end
+    it 'should not be valid without email' do
+      @user.email = nil
+      expect(@user).to_not be_valid
+    end
+    it 'should not be valid without password' do
+      @user.password = nil
+      expect(@user).to_not be_valid
+    end
+  end
 end
