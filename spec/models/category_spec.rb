b758d523bc32b83de5cfac2aade75e2d60dbdfda
Omar Ragheb <92755394+omar25ahmed@users.noreply.github.com>
2022-06-30 23:53:11 +0200

    Revert "Add unit tests"

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,21 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  describe 'validations' do
-    before(:each) do
-      @user = User.create(name: 'Jane Doe', email: 'a@a', password: '123456', confirmed_at: Time.now)
-      @category = Category.create(name: 'Sport', icon: 'icon', user_id: @user.id)
-    end
-    it 'should be valid' do
-      expect(@category).to be_valid
-    end
-    it 'should not be valid without name' do
-      @category.name = nil
-      expect(@category).to_not be_valid
-    end
-    it 'should not be valid without icon' do
-      @category.icon = nil
-      expect(@category).to_not be_valid
-    end
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,25 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'validations' do
-    before(:each) do
-      @user = User.create(name: 'Jane Doe', email: 'a@a', password: '123456', confirmed_at: Time.now)
-      @category = Category.create(name: 'Sport', icon: 'icon', user_id: @user.id)
-    end
-    it 'should be valid' do
-      expect(@user).to be_valid
-    end
-    it 'should not be valid without name' do
-      @user.name = nil
-      expect(@user).to_not be_valid
-    end
-    it 'should not be valid without email' do
-      @user.email = nil
-      expect(@user).to_not be_valid
-    end
-    it 'should not be valid without password' do
-      @user.password = nil
-      expect(@user).to_not be_valid
-    end
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
